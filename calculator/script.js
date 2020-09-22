d4f1538cf02505a0fd4cffd8911750cfe97cf801
Roman Kohutyak <romankohutyak@gmail.com>
2020-09-22 14:48:49 +0300

    feat: implement new buttons in layout

---
+++ calculator/script.js
@@ -0,0 +1,3 @@
+const screenTopNumber = document.querySelector('[data-previous-operand]'),
+  screenBottomNumber = document.querySelector('[data-current-operand]'), 
+  numbers = document.querySelectorAll('[data-number]'),
