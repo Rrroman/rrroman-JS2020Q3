4693f513dae48f1944d8891a4d2cef7f90c61311
Roman Kohutyak <romankohutyak@gmail.com>
2020-10-16 01:29:40 +0300

    feat: add burger to pets page

--- shelter/pages/pets/js/hamburger.js
+++ shelter/pages/pets/js/hamburger.js
@@ -1,9 +1,20 @@
 const hamburger = document.querySelector('.header__burger');
 const navList = document.querySelector('.nav__list');
-const friendsContainer = document.querySelector('.friends__container');
+const logo = document.querySelector('.logo');
+const logoTitle = document.querySelector('.logo__title');
+const logoSubtitle = document.querySelector('.logo__subtitle');
+const navLinkActive = document.querySelector('.nav__link_active');
+const container320 = document.querySelector('.container_320');
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('header__burger_active');
-  navList.classList.toggle('nav__list_active');
-  friendsContainer.classList.toggle('friends__container_320');
+let elementsArr = [navLinkActive, hamburger];
+
+elementsArr.forEach((element) => {
+  element.addEventListener('click', () => {
+    hamburger.classList.toggle('header__burger_active');
+    navList.classList.toggle('nav__list_active');
+    logo.classList.toggle('logo_active');
+    logoTitle.classList.toggle('logo__title_active');
+    logoSubtitle.classList.toggle('logo__subtitle_active');
+    container320.classList.toggle('container_320_active');
+  });
 });
