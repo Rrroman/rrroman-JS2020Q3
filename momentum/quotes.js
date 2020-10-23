df689bcfcbc330542e068643e5dac95d9efbeb2f
Roman Kohutyak <romankohutyak@gmail.com>
2020-10-23 18:37:29 +0300

    refactor: btn class to btn-quote-refresh

--- momentum/quotes.js
+++ momentum/quotes.js
@@ -1,12 +1,11 @@
 const blockquote = document.querySelector('blockquote');
 const figcaption = document.querySelector('figcaption');
-const btn = document.querySelector('.btn');
+const btn = document.querySelector('.btn-quote-refresh');
 
 async function getQuote() {
   const url = `https://favqs.com/api/qotd`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
   blockquote.textContent = data.quote.body;
   figcaption.textContent = data.quote.author;
 }
